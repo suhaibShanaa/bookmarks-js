a478acb0c4abdfcc1d08ff29779d9cd636edb4b2
SuhaibShanaa <suhaibsh612@gmail.com>
2021-10-10 04:42:48 +0300

    Bulid UI

---
+++ script.js
@@ -0,0 +1,6 @@
+const modal = document.getElementById('modalAddBookmark');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmarks-form');
+const websiteName = document.getElementById('website-name');
+const websiteURL = document.getElementById('website-url');
